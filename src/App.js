bc6eef0e933e9775802f1298d9ac6ddd1793d8df
Tony <wqtls1998@gmail.com>
2020-10-26 00:14:09 -0400

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import profileImage from './img/wordclouds.png'
+function App() {
+  return (
+    <div className="App">
+     <h1>My React App</h1>
+     <h2>Tony</h2>
+     <img src={profileImage} alt = "profile-image" />
+    </div>
+  );
+}
+
+export default App;
