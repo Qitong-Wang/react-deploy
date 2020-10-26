cb50a5ab996412660c65bbbdeb85c714774df5e9
Tony <wqtls1998@gmail.com>
2020-10-26 00:18:37 -0400

    test again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
+import logo from './logo.svg';
 import './App.css';
-import profileImage from './img/wordclouds.png'
+
 function App() {
   return (
     <div className="App">
-     <h1>My React App</h1>
-     <h2>Tony</h2>
-     <img src={profileImage} alt = "profile-image" />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
